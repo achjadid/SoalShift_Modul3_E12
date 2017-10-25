4fc1de62d7a0608260424fc65ad71b57b493c010
Unknown <fwildanf@gmail.com>
2017-10-25 13:55:08 +0700

    Finish

--- soal4.c
+++ soal4.c
@@ -5,8 +5,10 @@
 #include <string.h>
 
 int num[101];
+int cek=0;
 
 void* factorial(void *arg){
+	cek=0;
 	int *x=arg;
 	int res;
 	if(num[*x]==1) res=1;
@@ -15,6 +17,7 @@ void* factorial(void *arg){
 		for(int i=2;i<=num[*x];i++) res*=i;
 	}
 	printf("Hasil %d! = %d\n", num[*x],res);
+	cek=1;
 }
 
 int main(){
@@ -38,6 +41,15 @@ int main(){
 			}
 		}
 	}while(temp!='\n');
+	pthread_t tid[n];
+	int err;
+	for(int i=0;i<n;i++){
+		cek=0;
+		err=pthread_create(&(tid[i]),NULL,factorial,(void*)&i);
+		while(cek!=1){}
+	}
+	for(int j=0;j<n;j++) pthread_join(tid[j],NULL);
+	return 0;
 }
 
 
