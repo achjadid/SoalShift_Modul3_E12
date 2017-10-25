c86caa7ff5beaf6a99dd83aeadb5d26ba3f82a06
Unknown <fwildanf@gmail.com>
2017-10-25 14:28:48 +0700

    Status awal hewan + Opsi menu

---
+++ soal3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+int lohan=100;
+int kepiting=100;
+
+void menu(){
+	printf("Pilih menu :\n");
+	printf("1.Beri makan lohan\n");
+	printf("2.Beri makan kepiting\n");
+}
+
+int main(){
+	
+}
