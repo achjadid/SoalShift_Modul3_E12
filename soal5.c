ffd5da415b301813a1483f0ae079314b112659ba
jadidampme <achmadjadid10@gmail.com>
2017-10-25 14:06:15 +0700

    test3

--- soal5.c
+++ soal5.c
@@ -4,13 +4,14 @@
 #include<stdlib.h>
 #include<unistd.h> 
 
-int main()
+void* findword(void* arg)
 {	
-	int i=0;
+	int i=0, a;
 	char text[1500];
 	char *word;
 	FILE *novel;
 	scanf("%s",word);
+	a=strlen(word);
 	novel=fopen("Novel.txt","r");
 	while(fscanf(novel,"%s",text)!=EOF)
 	{
@@ -20,5 +21,26 @@ int main()
 		}
 	}
 	printf("%s: %d\n",word,i);
+	fclose(novel);
 	return 0;
+	
+}
+
+int main()
+{
+	int i;
+	int err;
+	pthread_t tid[2];	
+	for(i=0;i<2;i++)
+	{
+		err=pthread_create(&(tid[i]),NULL,&findword,NULL);
+		if(err!=0)
+	        {
+			printf("\n can't create thread : [%s]",strerror(err));
+		}
+
+	}
+	pthread_join(tid[0],NULL);
+	pthread_join(tid[1],NULL);
+    return 0;
 }
